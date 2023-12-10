0db55cccd1b2927ed5a5e9aa757d091348ed0493
Karinagth1 <karina_pineda97@hotmail.com>
2023-12-10 08:29:14 -0500

    change card router-controller

--- server/src/controllers/card.js
+++ server/src/controllers/card.js
@@ -3,21 +3,8 @@ const cardService = require('../services/card');
 const createCard = async (req, res) => {
     try {
         const { name, age, sex, date, description } = req.body;
-        const card = await cardService.saveNewCard(name, age, sex, date, description);
-        res.json(card);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-};
-
-const getCard = async (req, res) => {
-    try {
-        const cardId = req.params.id;
-        const card = await cardService.getCard(cardId);
-        if (!card) {
-            res.status(404).send('Card not found');
-        }
-        res.json(card);
+        await cardService.saveNewcard(name, age, sex, date, description);
+        res.status(201).send('New Card date added successfully')
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -25,25 +12,51 @@ const getCard = async (req, res) => {
 
 const getCards = async (req, res) => {
     try {
-        const cards = await cardService.getCards();
+        const cards = await cardService.getcards();
         res.json(cards);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
-const updateCard = async (req, res) => {
+const updateCard = async (req, res) => {//es para crear e insertar same time
     try {
-        const card = await cardService.updateCard(req.params.id, req.body);
-        res.json(card);
+        const cardId = req.params.id;
+        const {name,age,sex,date,description}=req.body
+        await cardService.updatecard(cardId,name,age, sex, date, description);
+        res.status(201).send('New Card date updated successfully')
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+const upsertCard = async (req, res) => {//es para editar
+    try {
+        const cardId = req.params.id;
+        const { name, age, sex, date, description } = req.body;
+        await cardService.upsertcard(cardId, name,age, sex, date, description);
+        res.status(204).send('New Card date updated successfully')
+    } catch (error) {
+        console.error(err)
+        res.status(500).json({ error: error.message });
+    }
+};
+const deleteCard = async (req, res) => {//es para editar
+    try {
+        const cardId = req.params.id;
+        await cardService.deletecard(cardId);
+        res.status(204).send('New Card date delete successfully')
+    } catch (error) {
+      
+        res.status(500).json({ error: error.message });
+    }
+};
+
 
 module.exports = {
     createCard,
-    getCard,
+    getCards,
     getCards,
     updateCard,
+    upsertCard,
+    deleteCard
 };
